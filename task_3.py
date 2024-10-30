5ba541caaa17786c2d86d4ce7feabd6c71a81e1c
timmedic <timmedic39322@mail.ru>
2024-10-30 15:20:54 +0000

    Исправил ошибки

--- task_3.py
+++ task_3.py
@@ -4,12 +4,24 @@ import random
 
 x0 = random.randint(0, 100)
 y0 = random.randint(0, 100)
+
 v0 = random.randint(0, 100)
 
-table = np.zeros((3, 6))
+#table = np.zeros((3, 6))
 
-for t in range(0, 6, 1):
-    table[0, t] = t
-    table[1, t] = x0 + v0 * t
-    table[2, t] = y0 + v0 * t - ((pc.g * (t ** 2)) / 2)
-    print(f"time) {t}", f"x) {table[1, t]}", f"y) {table[2, t]}", sep="\n")
+#for t in range(0, 6, 1):
+#    table[0, t] = t
+#    table[1, t] = x0 + v0 * t
+#    table[2, t] = y0 + v0 * t - ((pc.g * (t ** 2)) / 2)
+#    print(f"time) {t}", f"x) {table[1, t]}", f"y) {table[2, t]}", sep="\n")
+
+t = (np.arange(0, 6, 0.5))
+x = (x0 + v0) * t
+y = (y0 + v0) * t - ((pc.g * (t ** 2)) / 2)
+
+table = np.zeros((3, len(t)))
+table[0, :] = t[:]
+table[1, :] = x[:]
+table[2, :] = y[:]
+
+print(table)
