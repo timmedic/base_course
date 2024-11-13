0126224df993ef0fd39ee8d0ac14a7907376f778
timmedic <timmedic39322@mail.ru>
2024-11-13 14:24:03 +0000

    переделал пятую задачу под безводдную конструкцию

--- task_5.py
+++ task_5.py
@@ -1,6 +1,6 @@
 import math
 
-def GetSquare(shape: str):
+"""def GetSquare(shape: str):
     if(shape == "circle"):
         R = float(input("Введите радиус окружности: "))
         return math.pi * (R ** 2)
@@ -19,4 +19,28 @@ def GetSquare(shape: str):
 
 print("Доступные фигуры - circle, rectangle, triangle")
 shape = str(input("Выберите фигуру: "))
-print(GetSquare(shape))
+print(GetSquare(shape))"""
+
+def GetSquare(sizes: list):
+    if(len(sizes) == 1):
+        return math.pi * (sizes[0] ** 2)
+    elif(len(sizes) == 2):
+        return sizes[0] * sizes[1]
+    elif(len(sizes) == 3):
+        p = (sizes[0] + sizes[1] + sizes[2]) / 2
+        return math.sqrt(p * (p - sizes[0]) * (p - sizes[1]) * (p - sizes[2]))
+    else:
+        print("Не удалось распознать фигуру")
+
+def GetListOfInput():
+    inputList = []
+    symp = '0'
+    print('Вводите размеры фигуры, которой площадь вы хотите найти. По завершению ввода, введите "*"')
+    while(True):
+        inputWord = input(">> ")
+        if(inputWord == "*"):
+            return inputList
+        inputWord = float(inputWord)
+        inputList.append(inputWord)
+
+print(f"Площадь - {GetSquare(GetListOfInput())}")
